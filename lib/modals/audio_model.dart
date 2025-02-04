6f6ae831351bde33401e99eb3ba8ff18fbc3560d
JehadISB <jib143327@gmail.com>
2025-02-04 08:55:56 +0300

    create audio model

---
+++ lib/modals/audio_model.dart
@@ -0,0 +1,13 @@
+class AudioModel {
+  String surahName;
+  String readerName;
+  String soundPath;
+  int soundDuration;
+
+  AudioModel({
+    required this.surahName,
+    required this.readerName,
+    required this.soundPath,
+    required this.soundDuration,
+  });
+}
---
+++ lib/modals/list_surah.dart
@@ -0,0 +1,5 @@
+import 'package:ashbal_almihrab_app/modals/audio_model.dart';
+
+List<AudioModel> juzaAma = [
+  // AudioModel(surahName: "سورة الفاتحة", readerName: "مشعل الحداد", soundPath: ,soundDuration: )
+];
